22678b3ea4c8a3b4682ce14c1ac6f5c872ef7986
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 11:15:33 -0300

    Connected the Task schema for taskService layer

--- Back-end/src/services/taskService.js
+++ Back-end/src/services/taskService.js
@@ -1,6 +1,11 @@
 const Task = require('../models/taskModel');
 
+const Schema = require('../utils/schema');
+
 const create = async (body, userId) => {
+  const { error } = Schema.Task.validate(body);
+  if (error) return { status: 400, message: error.details[0].message };
+
   const newTask = await Task.create(body, userId);
 
   return { status: 201, data: newTask };
