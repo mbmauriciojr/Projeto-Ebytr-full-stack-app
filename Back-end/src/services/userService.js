604a92a4a38e4213e9fe60225b21ba784166fa3e
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 12:04:41 -0300

    Fix: to create a default return to userService

--- Back-end/src/services/userService.js
+++ Back-end/src/services/userService.js
@@ -1,14 +1,16 @@
 const User = require('../models/userModel');
 
 const Schema = require('../utils/schema');
+const tokenJWT = require('../middleware/auth/validateJWT');
 
 const create = async (body) => {
   const { error } = Schema.User.validate(body);
   if (error) return { status: 400, message: error.details[0].message };
 
   const newUser = await User.create(body);
+  const token = tokenJWT.createToken(newUser);
 
-  return { status: 201, data: newUser };
+  return { status: 201, data: { user: newUser, token } };
 };
 
 module.exports = {
