f358ae694fd86ab5349bbc458df13f8e987016cc
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 10:52:59 -0300

    Created an endpoint to create new users

--- Back-end/src/api/app.js
+++ Back-end/src/api/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const Tasks = require('../controllers/taskController');
+const Users = require('../controllers/userController');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/task', Tasks.create);
+
+app.post('/user', Users.create);
 
 module.exports = app;
