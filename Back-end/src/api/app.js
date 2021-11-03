7ee44257c26483cd0c1e645163b7c26add031cdf
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 10:23:17 -0300

    Created the endpoint to create a new taks on the database

--- Back-end/src/api/app.js
+++ Back-end/src/api/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const Tasks = require('../controllers/taskController');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.post('/task', Tasks.create);
 
 module.exports = app;
