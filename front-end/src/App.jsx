fa4f96371c6f2e39901f83dec62e9b6a5031dc2e
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 18:24:34 -0300

    Fix: change the files to works on JSX format because the Eslint

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Login from './pages/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
