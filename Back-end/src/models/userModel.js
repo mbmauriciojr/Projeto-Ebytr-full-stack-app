cde45d322bd2c753fb3daec67a7e1edc860869ca
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 13:21:18 -0300

    Created a fuction to check if a user email is register in the database

--- Back-end/src/models/userModel.js
+++ Back-end/src/models/userModel.js
@@ -1,5 +1,13 @@
 const connection = require('./connection');
 
+const findEmail = async (email) => {
+  const db = await connection();
+
+  const checkEmail = await db.collection('users').findOne({ where: { email } });
+
+  return checkEmail;
+};
+
 const create = async (body) => {
   const db = await connection();
 
@@ -8,6 +16,13 @@ const create = async (body) => {
   return { _id: createTask.insertedId, body };
 };
 
+const login = async (body) => {
+  const { email, password } = body;
+
+  const checkEmail = await findEmail(email);
+};
+
 module.exports = {
   create,
+  login,
 };
