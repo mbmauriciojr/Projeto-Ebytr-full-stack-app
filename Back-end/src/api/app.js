4f47c58c8c718724c9052d74fa6e52d2e604e0d8
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 13:31:04 -0300

    Created an endpoint to login

--- Back-end/src/api/app.js
+++ Back-end/src/api/app.js
@@ -4,11 +4,15 @@ const bodyParser = require('body-parser');
 const Tasks = require('../controllers/taskController');
 const Users = require('../controllers/userController');
 
+const validateJWT = require('../middleware/auth/validateJWT').verifyToken;
+
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/task', Tasks.create);
-
 app.post('/user', Users.create);
+
+app.post('/login', Users.login);
+
+app.post('/task', validateJWT, Tasks.create);
 
 module.exports = app;
