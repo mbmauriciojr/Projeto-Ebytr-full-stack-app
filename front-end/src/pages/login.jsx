dd81b7e7a80903c1049d4d9dae22951735c36cc3
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 18:18:32 -0300

    Fix: Eslint config

---
+++ front-end/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <>
+      <p>Tela de Login</p>
+      <div id="login-form">
+        <input type="text" placeholder="email" name="email" />
+        <input type="text" placeholder="password" name="password" />
+      </div>
+    </>
+  );
+}
+
+export default Login;
