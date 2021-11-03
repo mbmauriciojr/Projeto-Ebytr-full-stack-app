862f1825fe3886c9926fb884124f9608e14bad8c
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-03 10:52:33 -0300

    Fix: now  mode to insert in mongoDB is correct

--- Back-end/src/models/userModel.js
+++ Back-end/src/models/userModel.js
@@ -3,7 +3,7 @@ const connection = require('./connection');
 const create = async (body) => {
   const db = await connection();
 
-  const createTask = await db.collection('users').inserOne({ ...body });
+  const createTask = await db.collection('users').insertOne({ ...body });
 
   return { _id: createTask.insertedId, body };
 };
