6180da6ec08a16013a61d43fa26a1541ae501871
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-04 15:21:26 -0300

    Created a function where render all task for the user

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,13 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { object } from 'prop-types';
+import { userTasksFetch } from '../services/tasksAPI';
 import ContextTasks from '../provider/ContextTasks';
 
 function Home() {
   const { user } = useContext(ContextTasks);
-  console.log(user);
+  const { _id: id } = user;
+
+  const [tasks, setTasks] = useState('');
+
+  const fetchTasks = async (idUser) => {
+    const userTasks = await userTasksFetch(idUser);
+
+    setTasks(userTasks);
+  };
+
+  const createTasks = () => (
+    tasks.map((task) => {
+      const { _id: idTask } = task;
+      return (<li key={idTask}>{task.taskName}</li>);
+    })
+  );
+
+  useEffect(() => {
+    fetchTasks(id);
+  }, []);
+
   return (
     <>
       <h1>{user.displayName}</h1>
+      <ul>
+        { tasks.length === 0 ? null : createTasks() }
+      </ul>
     </>
   );
 }
