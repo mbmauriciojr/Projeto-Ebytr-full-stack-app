15c8a22bb491da2b5f31c1d4c892ff12e1e0ce03
Marcelo Maurício <mmauricio.design@gmail.com>
2021-11-04 10:19:52 -0300

    Created a Login page where user can log or click to create a new

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { object } from 'prop-types';
 
 function Login() {
   return (
@@ -6,10 +8,21 @@ function Login() {
       <p>Tela de Login</p>
       <div id="login-form">
         <input type="text" placeholder="email" name="email" />
-        <input type="text" placeholder="password" name="password" />
+        <input type="text" placeholder="senha" name="password" />
+        <button type="submit">Entrar</button>
+      </div>
+      <div id="create-new-user">
+        <p>
+          Caso queira criar um novo usuário clique
+          <Link to="/create-user">aqui</Link>
+        </p>
       </div>
     </>
   );
 }
+
+Login.propTypes = {
+  history: object,
+}.isRequired;
 
 export default Login;
